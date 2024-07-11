9780eb1689d0b50e8cf0fc498061d8f2814ff8a1
kubealex <al.rossi87@gmail.com>
2024-07-11 16:30:32 +0200

    cleanup

--- lab-setup/terraform/.terraform/modules/satellite_instances.satellite_instance/modules/terraform-libvirt-instance/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
- required_version = ">= 1.0"
-  required_providers {
-    libvirt = {
-      source  = "dmacvicar/libvirt"
-      version = "0.7.6"
-    }
-  }
-}
--- lab-setup/terraform/.terraform/modules/satellite_instances.satellite_instance/modules/terraform-libvirt-pool/main.tf
+++
@@ -1,5 +0,0 @@
-resource "libvirt_pool" "vm_pool" {
-  name = var.pool_name
-  type = "dir"
-  path = "${var.pool_path}/${var.pool_name}"
-}
